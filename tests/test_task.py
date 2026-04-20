a5d8ed163e382091d3f1f83f81060310e5da4cd9
konstantin <yetifor@mail.ru>
2026-04-20 19:16:13 +0300

    changed the logic and the subject being tested

--- tests/test_task.py
+++ tests/test_task.py
@@ -2,26 +2,20 @@ from playwright.sync_api import expect, Page
 from faker import Faker
 
 fake = Faker()
-ADRESS = 'http://localhost:7080'
+ADRESS = 'http://localhost:5000/'
 
 
 def test_login_loader(page: Page):
     page.goto(ADRESS)
-    page.get_by_role("link", name="Dynamic Loading").click()
-    page.wait_for_url("**/dynamic_loading")
-    page.get_by_role("link", name="Example 1: Element on page").click()
-    page.wait_for_url("**/dynamic_loading/1")
-    page.get_by_role("button", name="Start").click()
-    loader = page.locator("#loading")
+    page.get_by_test_id("nav-login").click()
+    page.wait_for_url("**/login")
+    page.get_by_test_id("login-username").fill(fake.user_name())
+    page.get_by_test_id("login-password").fill(fake.password())
+    page.get_by_test_id("login-submit").click()
+    loader = page.get_by_test_id("login-submit-spinner")
     expect(loader).to_be_visible()
     page.wait_for_timeout(10000)
     expect(loader).to_be_hidden()
-    page.goto(ADRESS)
     page.wait_for_timeout(5000)
-    page.get_by_role('link', name='Form Authentication').click()
-    page.wait_for_url('**/login')
-    page.get_by_role('textbox', name='Username').fill(fake.user_name())
-    page.get_by_role('textbox', name='Password').fill(fake.password())
-    page.get_by_role('button', name=' Login').click()
-    assert page.get_by_text('Your username is invalid!').is_visible(), \
-        'Сообщение "Your username is invalid!" не отображетсяя'
+    assert page.get_by_text('Invalid login or password.').is_visible(), \
+        'Сообщение "Invalid login or password." не отображетсяя'
